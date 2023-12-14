2824cc835cd19cfabc2d525848b99839e9d86411
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2023-12-15 01:36:55 +0900

    csv 구조 검증

---
+++ Data_pre.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import re
+import matplotlib.pyplot as plt
+
+train = 'train.txt'
+
+train_df = pd.read_csv(train)
+
+print(train_df.info())
--- regulate_text.py
+++ regulate_text.py
@@ -3,7 +3,7 @@ file_path = 'train.txt'
 with open(file_path, 'r', encoding='utf-8') as file:
     lines = file.readlines()
 
-processed_lines = [line.strip().strip('"') for line in lines]
+processed_lines = [line.strip().strip('"').strip(',') for line in lines]
 
 with open(file_path, 'w', encoding='utf-8') as file:
     for line in processed_lines:
