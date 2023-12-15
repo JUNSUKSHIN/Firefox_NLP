5befe3b256b9371d11ecb3559373f03ffc00676d
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2023-12-15 17:52:27 +0900

    학습한 모델 로드

--- Run_nlp.py
+++ Run_nlp.py
@@ -12,6 +12,143 @@ from transformers import BertModel
 import torch
 from torch import nn
 
+device_type = 'cuda' if torch.cuda.is_available() else 'cpu'
+device = torch.device(device_type)
+
+tokenizer = KoBERTTokenizer.from_pretrained('skt/kobert-base-v1')
+vocab = nlp.vocab.BERTVocab.from_sentencepiece(tokenizer.vocab_file, padding_token='[PAD]')
+bertmodel = BertModel.from_pretrained('skt/kobert-base-v1', return_dict=False)
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, vocab, max_len,
+                pad, pair):
+        transform = BERTSentenceTransform(bert_tokenizer, max_seq_length=max_len,vocab=vocab, pad=pad, pair=pair)
+        #transform = nlp.data.BERTSentenceTransform(
+        #    tokenizer, max_seq_length=max_len, pad=pad, pair=pair)
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+    
+class BERTSentenceTransform:
+    r"""BERT style data transformation.
+
+    Parameters
+    ----------
+    tokenizer : BERTTokenizer.
+        Tokenizer for the sentences.
+    max_seq_length : int.
+        Maximum sequence length of the sentences.
+    pad : bool, default True
+        Whether to pad the sentences to maximum length.
+    pair : bool, default True
+        Whether to transform sentences or sentence pairs.
+    """
+
+    def __init__(self, tokenizer, max_seq_length,vocab, pad=True, pair=True):
+        self._tokenizer = tokenizer
+        self._max_seq_length = max_seq_length
+        self._pad = pad
+        self._pair = pair
+        self._vocab = vocab
+
+    def __call__(self, line):
+        
+        text_a = line[0]
+        if self._pair:
+            assert len(line) == 2
+            text_b = line[1]
+
+        tokens_a = self._tokenizer.tokenize(text_a)
+        tokens_b = None
+
+        if self._pair:
+            tokens_b = self._tokenizer(text_b)
+
+        if tokens_b:
+            # Modifies `tokens_a` and `tokens_b` in place so that the total
+            # length is less than the specified length.
+            # Account for [CLS], [SEP], [SEP] with "- 3"
+            self._truncate_seq_pair(tokens_a, tokens_b,
+                                    self._max_seq_length - 3)
+        else:
+            # Account for [CLS] and [SEP] with "- 2"
+            if len(tokens_a) > self._max_seq_length - 2:
+                tokens_a = tokens_a[0:(self._max_seq_length - 2)]
+
+        # The embedding vectors for `type=0` and `type=1` were learned during
+        # pre-training and are added to the wordpiece embedding vector
+        # (and position vector). This is not *strictly* necessary since
+        # the [SEP] token unambiguously separates the sequences, but it makes
+        # it easier for the model to learn the concept of sequences.
+
+        # For classification tasks, the first vector (corresponding to [CLS]) is
+        # used as as the "sentence vector". Note that this only makes sense because
+        # the entire model is fine-tuned.
+        #vocab = self._tokenizer.vocab
+        vocab = self._vocab
+        tokens = []
+        tokens.append(vocab.cls_token)
+        tokens.extend(tokens_a)
+        tokens.append(vocab.sep_token)
+        segment_ids = [0] * len(tokens)
+
+        if tokens_b:
+            tokens.extend(tokens_b)
+            tokens.append(vocab.sep_token)
+            segment_ids.extend([1] * (len(tokens) - len(segment_ids)))
+
+        input_ids = self._tokenizer.convert_tokens_to_ids(tokens)
+
+        # The valid length of sentences. Only real  tokens are attended to.
+        valid_length = len(input_ids)
+
+        if self._pad:
+            # Zero-pad up to the sequence length.
+            padding_length = self._max_seq_length - valid_length
+            # use padding tokens for the rest
+            input_ids.extend([vocab[vocab.padding_token]] * padding_length)
+            segment_ids.extend([0] * padding_length)
+
+        return np.array(input_ids, dtype='int32'), np.array(valid_length, dtype='int32'),\
+            np.array(segment_ids, dtype='int32')
+    
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                bert,
+                hidden_size = 768,
+                num_classes = 4, 
+                dr_rate = None,
+                params = None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p = dr_rate)
+
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device),return_dict = False)
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+    
+model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
+model.load_state_dict(torch.load(r'C:\137\trained_model.pt', map_location=device))
+model.eval()
 
 w_model = whisper.load_model("base")
 WAVE_OUTPUT_FILENAME = r"C:\137\output.wav"
@@ -65,6 +202,8 @@ def main():
     transcription = transcribe_audio(WAVE_OUTPUT_FILENAME)
     print("변환된 텍스트:")
     print(transcription)
+
+
 
 if __name__ == "__main__":
     main()
