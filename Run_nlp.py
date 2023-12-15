2e3026d0a8bf9970a2e07bfeb6b8579b34bfca1f
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2023-12-15 15:56:35 +0900

    녹음 환경설정 세팅

--- Run_nlp.py
+++ Run_nlp.py
@@ -1,3 +1,21 @@
 import pyaudio
 import wave
 import subprocess
+
+def record_audio():
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    CHUNK = 1024
+    RECORD_SECONDS = 3
+    DEVICE_INDEX = 2  # 마이크 장치 인덱스
+    WAVE_OUTPUT_FILENAME = "output.wav"
+
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open(format=FORMAT, channels=CHANNELS,
+                        rate=RATE, input=True, input_device_index=DEVICE_INDEX,
+                        frames_per_buffer=CHUNK)
+
+    print("녹음 시작...")
+    frames = []
