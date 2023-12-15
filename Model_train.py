b4544182a27bd310219e56dbcb7ae83edd80b1d6
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2023-12-15 13:11:24 +0900

    머신러닝 반복 코드

--- Model_train.py
+++ Model_train.py
@@ -263,3 +263,43 @@ def calc_accuracy(X,Y):
     max_vals, max_indices = torch.max(X, 1)
     train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
     return train_acc
+
+
+train_history = []
+test_history = []
+loss_history = []
+
+for e in range(num_epochs):
+    train_acc = 0.0
+    test_acc = 0.0
+    model.train()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm(train_dataloader)):
+        optimizer.zero_grad()
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+
+        loss = loss_fn(out, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+        optimizer.step()
+        scheduler.step()  
+        train_acc += calc_accuracy(out, label)
+        if batch_id % log_interval == 0:
+            print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+            train_history.append(train_acc / (batch_id+1))
+            loss_history.append(loss.data.cpu().numpy())
+    print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
+
+    model.eval()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm(test_dataloader)):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length = valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        test_acc += calc_accuracy(out, label)
+    print("epoch {} test acc {}".format(e+1, test_acc / (batch_id+1)))
+    test_history.append(test_acc / (batch_id+1))
