073b396936ceba3d52ed0f8d0c5554a31ff9a072
JUNSUKSHIN <junsuk2017@chungbuk.ac.kr>
2023-12-15 19:01:17 +0900

    데이터 시각화 추가

--- Data_pre.py
+++ Data_pre.py
@@ -13,3 +13,13 @@ with pd.option_context('display.max_colwidth', -1):
 # 기본 정보 출력
 print(train_df.info())
 print(train_df.head())
+
+# 'target' 열의 빈도 수 계산
+target_counts = train_df['target'].value_counts()
+
+# 막대 그래프로 시각화
+target_counts.plot(kind='bar')
+plt.title('Target Value Counts')
+plt.xlabel('Target')
+plt.ylabel('Counts')
+plt.show()
